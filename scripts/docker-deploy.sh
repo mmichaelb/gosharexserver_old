ea694c9b0f6cec80b1ab15f6e570aefb6dbe2ea5
mmichaelb <mmichaelb@protonmail.com>
2018-11-15 22:15:27 +0100

    Update docker deploy file

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 make build-docker
 docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+# build gosharexserver image
 export REPO=mmichaelb/gosharexserver
-docker build -f ./build/package/Dockerfile -t ${REPO}:$1 .
+docker build -f ./build/gosharexserver/Dockerfile -t ${REPO}:$1 .
+docker tag ${REPO}:$1 ${REPO}:latest
+docker push ${REPO}
+# build gosharexserver-user-adder tool image
+export REPO=mmichaelb/gosharexserver-user-adder
+docker build -f ./build/gosharexserver-user-adder/Dockerfile -t ${REPO}:$1 .
 docker tag ${REPO}:$1 ${REPO}:latest
 docker push ${REPO}
